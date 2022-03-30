ab78d158818c26b08a51a7ecb00b984aed4af595
Caio Andrade Mota <cam.snf19@uea.edu.br>
2022-03-30 11:32:01 -0400

    streamlit

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from pearson import get_recommendation
+
+st.title('Sistema de Recomendação de Vinhos')
+st.subheader("Sistema de recomendação que utiliza o Streamlit para renderização de interfaces no front-end, no back-end, utiliza Python, e o Algoritmo de recomendação Pearson.")
+
+
+form = st.form("my_form")
+username = form.text_input(
+      'Insira o nome de usuário para verificar as recomendações de vinhos',
+      value = '',
+      max_chars = 30
+)
+
+# Every form must have a submit button.
+submitted = form.form_submit_button("Submit")
+if submitted:
+      get_recommendation(username)
